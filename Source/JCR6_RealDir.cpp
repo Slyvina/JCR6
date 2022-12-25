25883a2f313bf54afb3f02482e3b172462172795
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-25 20:43:13 +0100

    Enhancement: Function to always read a dir as JCR6
    (and not scan for any other possibilities)

--- Source/JCR6_RealDir.cpp
+++ Source/JCR6_RealDir.cpp
@@ -1,7 +1,27 @@
+// Lic:
+// JCR6/Source/JCR6_RealDir.cpp
+// Slyvina - JCR6 - Driver - Real Directory
+// version: 22.12.25
+// Copyright (C) 2022 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
 #include <JCR6_Core.hpp>
 #include <SlyvStream.hpp>
 #include <SlyvDir.hpp>
 #include <SlyvString.hpp>
+#include "JCR6_RealDir.hpp"
 
 namespace Slyvina {
 	namespace JCR6 {
@@ -40,6 +60,16 @@ namespace Slyvina {
 			RD.Recognize = RDRec;
 			RD.Dir = RDDir;
 			RegisterDirDriver(RD);
+		}
+
+		JT_Dir GetDirAsJCR(std::string path, bool AllowMerge) {
+			using namespace Units;
+			if (!DirectoryExists(path)) return nullptr;
+			auto tm{ JCR6_RealDir_AutoMerge };
+			JCR6_RealDir_AutoMerge = AllowMerge;
+			auto ret = RDDir(path, "");
+			JCR6_RealDir_AutoMerge = tm;
+			return ret;
 		}
 
 	}
