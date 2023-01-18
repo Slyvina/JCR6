0519375d3d1ad904a9c2ef8862f4c7141715150e
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-01-18 21:14:50 +0100

    vUpdate

--- Source/JCR6_JQL.cpp
+++ Source/JCR6_JQL.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // JCR6/Source/JCR6_JQL.cpp
 // JCR Quick Link
-// version: 23.01.10
+// version: 23.01.17
 // Copyright (C) 2023 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
