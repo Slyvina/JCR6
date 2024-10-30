cc3e96bc21bc4fb6095bdd768750b3be003b7f63
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-30 10:54:45 +0100

    Update JCR6_JQL.cpp

--- Source/JCR6_JQL.cpp
+++ Source/JCR6_JQL.cpp
@@ -1,3 +1,26 @@
+// License:
+// 	JCR6/Source/JCR6_JQL.cpp
+// 	JCR Quick Link
+// 	version: 24.10.20
+// 
+// 	Copyright (C) 2023, 2024 Jeroen P. Broks
+// 
+// 	This software is provided 'as-is', without any express or implied
+// 	warranty.  In no event will the authors be held liable for any damages
+// 	arising from the use of this software.
+// 
+// 	Permission is granted to anyone to use this software for any purpose,
+// 	including commercial applications, and to alter it and redistribute it
+// 	freely, subject to the following restrictions:
+// 
+// 	1. The origin of this software must not be misrepresented; you must not
+// 	   claim that you wrote the original software. If you use this software
+// 	   in a product, an acknowledgment in the product documentation would be
+// 	   appreciated but is not required.
+// 	2. Altered source versions must be plainly marked as such, and must not be
+// 	   misrepresented as being the original software.
+// 	3. This notice may not be removed or altered from any source distribution.
+// End License
 // Lic:
 // JCR6/Source/JCR6_JQL.cpp
 // JCR Quick Link
@@ -325,8 +348,12 @@ namespace Slyvina { //namespace UseJCR6 {
 							if (tg.size() > 1 && tg[1] == ':') tg = tg.substr(2);
 							while (tg[1] == '/') tg = tg.substr(1);
 							if (rw == "") throw runtime_error("STEAL no entry");
-							if (tg == "") throw runtime_error("STEALno target");
+							if (tg == "") throw runtime_error("STEAL no target");
 							auto ei{ From->Entry(rw) };
+							if (!ei) {
+								std::cout << "\x1b[31mERROR:\x1b[0m Entry to steal turns out to be a null pointer(Entry:" << rw << " from target : " << tg << ")\n";
+								throw std::runtime_error("Entry to steal turns out to be a null pointer (Entry:" + rw + " from target:" + tg + ")"); 
+							}
 							auto eor{ new _JT_Entry() }; *eor = *ei;
 							auto eo{ shared_ptr<_JT_Entry>(eor) };
 							eo->_ConfigString["__Entry"] = fpath + tg;
